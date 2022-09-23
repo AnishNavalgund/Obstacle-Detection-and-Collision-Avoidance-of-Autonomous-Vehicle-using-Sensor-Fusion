43a8936cf04d90c4b726596558a2124f043eab1c
AnishNavalgund <107324261+AnishNavalgund@users.noreply.github.com>
2022-09-23 20:35:12 +0200

    Add files via upload
    Adding project files

---
+++ ir.ino
@@ -0,0 +1,23 @@
+
+int obstaclePin = 7;  // This is our input pin
+int hasObstacle = HIGH;  // HIGH MEANS NO OBSTACLE
+
+void setup() {
+
+  pinMode(obstaclePin, INPUT);
+  Serial.begin(9600);  
+}
+void loop() {
+  hasObstacle = digitalRead(obstaclePin); //Reads the output of the obstacle sensor from the 7th PIN of the Digital section of the arduino
+  if (hasObstacle == LOW) //LOW means something is ahead, so illuminates the 13th Port connected LED
+  {
+    Serial.println("Stop something is ahead!!");
+    
+  }
+  else
+  {
+    Serial.println("Path is clear");
+   
+  }
+ delay(500);
+}
